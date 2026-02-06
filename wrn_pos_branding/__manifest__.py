0ba68b2d38918b266e960b243430bb9990885127
warancloud <a.cali@warancloud.com>
2026-02-06 18:35:12 +0300

    feat: remove Get QR Codes buttons

--- wrn_pos_branding/__manifest__.py
+++ wrn_pos_branding/__manifest__.py
@@ -10,7 +10,7 @@
     """,
     'author': 'WaranCloud',
     'website': 'https://www.warancloud.com',
-    'depends': ['point_of_sale'],
+    'depends': ['point_of_sale', 'pos_self_order'],
     'data': [
         'views/pos_assets_index.xml',
     ],
